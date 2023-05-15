74665bc42ba859039fe71da138ac1a4c22649dac
saimersaavedra <saimeradriansr@ufps.edu.co>
2023-05-14 22:35:56 -0500

    creo entity

---
+++ FarmApp/src/main/java/Model/Entity/Farmaceutico.java
@@ -0,0 +1,127 @@
+package Model.Entity;
+
+public class Farmaceutico {
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String correo;
+    private String direccion;
+    private String nivel;
+
+    public Farmaceutico() {
+    }
+
+    public Farmaceutico(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public Farmaceutico(String cedula, String nombre, String apellido, String telefono, String correo, String direccion, String nivel) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.direccion = direccion;
+        this.nivel = nivel;
+    }
+
+    /**
+     * @return the cedula
+     */
+    public String getCedula() {
+        return cedula;
+    }
+
+    /**
+     * @param cedula the cedula to set
+     */
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the nivel
+     */
+    public String getNivel() {
+        return nivel;
+    }
+
+    /**
+     * @param nivel the nivel to set
+     */
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+}
