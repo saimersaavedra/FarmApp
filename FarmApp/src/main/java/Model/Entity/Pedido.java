664f39fa825f4cb3963ab334ebc29e9c4bb9fc22
saimersaavedra <saimeradriansr@ufps.edu.co>
2023-05-21 20:12:08 -0500

    se realizaron las entidades

--- FarmApp/src/main/java/Model/Entity/Pedido.java
+++ FarmApp/src/main/java/Model/Entity/Pedido.java
@@ -1,5 +1,82 @@
 package Model.Entity;
 
+import java.sql.Date;
+
 public class Pedido {
+    private int pedido;
+    private String direccion;
+    private String cliente;
+    private Date Fecha;
+    private float total;
+
+    /**
+     * @return the pedido
+     */
+    public int getPedido() {
+        return pedido;
+    }
+
+    /**
+     * @param pedido the pedido to set
+     */
+    public void setPedido(int pedido) {
+        this.pedido = pedido;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the cliente
+     */
+    public String getCliente() {
+        return cliente;
+    }
+
+    /**
+     * @param cliente the cliente to set
+     */
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    /**
+     * @return the Fecha
+     */
+    public Date getFecha() {
+        return Fecha;
+    }
+
+    /**
+     * @param Fecha the Fecha to set
+     */
+    public void setFecha(Date Fecha) {
+        this.Fecha = Fecha;
+    }
+
+    /**
+     * @return the total
+     */
+    public float getTotal() {
+        return total;
+    }
+
+    /**
+     * @param total the total to set
+     */
+    public void setTotal(float total) {
+        this.total = total;
+    }
     
 }
