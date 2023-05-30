780810aab0ecbcce4838ce21ba423d08f90df60d
saimersaavedra <saimeradriansr@ufps.edu.co>
2023-05-30 10:23:20 -0500

    se añade base de datos localhost

---
+++ FarmApp/src/main/java/red/BaseDatosLocal.java
@@ -0,0 +1,46 @@
+package red;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class BaseDatosLocal {
+      public static final String JDBC_URL = "jdbc:mysql://localhost:8081/FarmApp?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    public static final String DB_USER = "root";
+    public static final String DB_CLAVE = "1234";
+    
+    public static DataSource getDataSource(){
+        
+        BasicDataSource bs = new BasicDataSource();
+        bs.setUrl(JDBC_URL);
+        bs.setUsername(DB_USER);
+        bs.setPassword(DB_CLAVE);  
+        bs.setInitialSize(5);
+        return bs;       
+        
+    }
+    public static Connection getConnection() throws SQLException, ClassNotFoundException{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return getDataSource().getConnection();
+    }
+    
+    public static void close(Connection con) throws SQLException{
+        con.close();
+    }
+    
+    public static void close(Statement stm) throws SQLException{
+        stm.close();
+    }
+    
+    public static void close(ResultSet res) throws SQLException{
+        res.close();
+    }
+    
+    public static void close(PreparedStatement ps) throws SQLException{
+        ps.close(); 
+    }
+}
